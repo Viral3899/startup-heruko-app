43b6b599166e3fdad9e90d25fc6cb508a9005047
Viral3899 <viralsherathiya1008@gmail.com>
2022-05-02 01:00:13 +0530

    App Added

--- startup_app.py
+++ startup_app.py
@@ -32,7 +32,7 @@ else:
         
         founded_at=st.sidebar.selectbox("founded_at",(2007, 2008, 2003, 2011, 2009, 2012, 2004, 2006, 1983, 2001, 2002,1998, 1978, 2010, 1999, 1997, 2005, 1991, 2000, 1984, 1995, 1948,1996, 1993, 1980, 1986, 1921, 1989, 1967, 1920, 1979, 1969, 1992,1994, 1987, 1957, 1954, 1976, 1901, 1982, 1988, 1975, 1933, 1990,1953, 1919, 1973, 2013, 1962, 1970, 1974, 1985, 1981, 1935, 1968,1977, 1938, 1946, 1960, 1952, 1906, 1959, 1943, 1927, 1942, 1934,1932, 1947, 1972, 1924, 1955, 1922, 1917, 1965, 1971, 1964, 1961,1945, 1903, 1910, 1907, 1908, 1963, 1916, 1939, 1915, 1958, 1923,1956, 1949, 1940, 1950, 1914, 2014, 1926, 1929, 1936, 1966, 1937,1928, 1912, 1944, 1913, 1941, 1911, 1931, 1918, 1951, 1925, 1904,1909, 1930, 1902))
         
-        country_code=st.sidebar.selectbox("Country_code", ('other', 'USA', 'AUS', 'IND', 'FRA', 'ISR', 'GBR','NLD', 'DEU','CAN', 'ESP'))
+        country_code=st.sidebar.selectbox("Country_code", ('other', 'USA', 'AUS', 'IND', 'FRA', 'ISR', 'GBR', 'DEU','CAN', 'ESP'))
         
         funding_round=st.sidebar.slider("Funding_Round",1,4,1)
         
@@ -73,7 +73,7 @@ a=(pd.DataFrame(df['category_code'].value_counts()[15:].reset_index())['index'].
 catcode=df['category_code'].replace(a,'other')
 catcode=pd.get_dummies(catcode,prefix='category_code')
 
-b=(pd.DataFrame(df['country_code'].value_counts()[10:].reset_index())['index'].to_list())
+b=(pd.DataFrame(df['country_code'].value_counts()[9:].reset_index())['index'].to_list())
 countrycode=df['country_code'].replace(b,'other')
 countrycode=pd.get_dummies(countrycode,prefix='country_code')
 
